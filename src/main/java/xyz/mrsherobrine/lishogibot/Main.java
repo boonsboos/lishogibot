7fc6cb80715691d4a9120e88d245f0195930aad3
mrsherobrine <mrsherobrinenaomi@gmail.com>
2021-08-25 20:06:15 +0200

    yes

---
+++ src/main/java/xyz/mrsherobrine/lishogibot/Main.java
@@ -0,0 +1,15 @@
+package xyz.mrsherobrine.lishogibot;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class Main {
+
+    public ObjectMapper objectMapper = new ObjectMapper();
+
+    public static void main(String[] args) {
+
+        JDA jda = JDABuilder.createLight();
+
+    }
+}
